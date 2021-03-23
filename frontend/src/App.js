2588c3f74f3b1d02bf30a55bb43849a87f2a4b48
Nikhil Gautam <nikhilgautam992@gmail.com>
2021-03-23 19:03:00 +0530

    few docker fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import _ from "lodash";
 
 function App() {
+  console.log(_.add(2, 3));
+
   return (
     <div className="App">
       <header className="App-header">
